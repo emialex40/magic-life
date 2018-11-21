bae5157859e6abc235f64950a8ef0d9a34203173
emialex40 <26emic73@gmail.com>
2018-11-21 18:44:55 +0200

    edit mobile 21.11.2018 18.44

--- src/js/common.js
+++ src/js/common.js
@@ -10,40 +10,24 @@ $(document).ready(function () {
         modal.fadeOut(600);
     });
 
+    $('input[type="tel"]').inputmask('+7(999)999-99-99');
+
     //send form
     $('#form').submit(function (e) {
         e.preventDefault();
 
-        var name = $('input[name="name"]'),
-            phone = $('input[name="phone"]'),
-            regName = /^[a-zA-Zа-яА-Я]+$/;
-
-        if (name.val() !== '') {
-            if (regName.test(name.val())) {
-                if(phone.val() !== '') {
-                    var form_data = $(this).serialize();
-                    $.ajax({
-                        type: 'POST',
-                        url: 'send.php',
-                        data: form_data,
-                        success: function() {
-                            location.reload();
-                        },
-                        error: function () {
-                            alert('При отправке сообщения произошла ошибка!');
-                        }
-                    });
-                } else {
-                    alert('Поле "Телефон обязательно для заполнения!"');
-                }
-
-            }else {
-                alert('В поле "Имя" рарешено вводить только буквы!');
+        var form_data = $(this).serialize();
+        $.ajax({
+            type: 'POST',
+            url: 'send.php',
+            data: form_data,
+            success: function () {
+                alert("success")
+                location.reload();
+            },
+            error: function () {
+                alert('При отправке сообщения произошла ошибка!');
             }
-        } else {
-            alert('Поле "Имя" обязательно для заполнения!');
-        }
+        });
     });
-
-
 });
